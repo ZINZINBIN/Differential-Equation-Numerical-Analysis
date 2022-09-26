48097264b7e375a45c409ff1174aee3949fe31ee
zinzinbin <asdwlstn@naver.com>
2022-09-26 07:21:55 +0000

    update homework1 : finished

--- HW01/homework.py
+++ HW01/homework.py
@@ -20,21 +20,27 @@ Compare your results while increasing the node number for the integration of y =
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-from typing import List, Callable, Union
+from typing import List, Callable, Union, Literal
 
 # nodes generator : random / uniform / chebshev
+# random nodes
 def generate_random_nodes(n_nodes : int, lower : float, upper : float):
     result = np.random.rand(n_nodes)
     result = lower + (upper - lower) * result
     return result
 
+# uniform nodes
 def generate_uniform_nodes(n_nodes : int, lower : float, upper : float):
     result = np.array([i / n_nodes for i in range(n_nodes)])
     result = lower + (upper - lower) * result
     return result
 
-def generate_chebyshev_nodes(n_nodes : int, lower : float, upper : float):
-    indices = [math.pi * idx / n_nodes for idx in range(0,n_nodes)]
+# chebyshev nodes
+def generate_chebyshev_nodes(n_nodes : int, lower : float, upper : float, mode_type : Literal['type_1', 'type_2'] = 'type_1'):
+    if mode_type == 'type_1':
+        indices = [math.pi * idx / n_nodes for idx in range(0,n_nodes)]
+    else:
+        indices = [math.pi * (2 * idx + 1) / (2 * n_nodes + 1) for idx in range(0,n_nodes)]
     result = (lower + upper)/2  + (upper - lower)/2 * np.cos(indices)
     return sorted(result)
 
@@ -45,6 +51,8 @@ def func(x : float, lower : float = 0, upper : float = 2.0)->float:
     else:
         ValueError("x should be in range of [{},{}]".format(lower, upper))
 
+# compute polynomial 
+# p_k(x) = a_0 + a_1 * (x-x_0) + a_2 * (x-x_0) * (x-x_1) + ....
 def compute_polynomial(x : float, coeffs : List, x_data : List):
     n = len(coeffs)
     result = coeffs[0]
@@ -56,6 +64,16 @@ def compute_polynomial(x : float, coeffs : List, x_data : List):
         
     return result
 
+# compute polynomial with multiple data
+# x : [x_0, x_1, ...]
+# output : [p_k(x_0), p_k(x_1), ...]
+def compute_polynomials(xs : Union[List, np.array], coeffs : List, x_data : List):
+    y = [] 
+    for x in xs:
+        y.append(compute_polynomial(x, coeffs, x_data))
+    return np.array(y)
+
+# compute Newton interpolation coefficient
 def compute_coeffs(func : Callable[[float], float], x_data : Union[List[float], np.array]):
     y_data = [func(x) for x in x_data]
     y_data = np.array(y_data)
@@ -73,33 +91,24 @@ def compute_coeffs(func : Callable[[float], float], x_data : Union[List[float], 
             mat[idx_j, idx_i] = dy / dx
     
     coeffs = np.diag(mat)
-
     return coeffs
 
 if __name__ == "__main__":
 
-    n_nodes = 8
+    n_nodes = 4
     lower = 0
     upper = 2
 
-    nodes_cheb = generate_chebyshev_nodes(n_nodes, lower, upper)
+    nodes_cheb = generate_chebyshev_nodes(n_nodes, lower, upper, 'type_2')
     nodes_uniform = generate_uniform_nodes(n_nodes, lower, upper)
 
     coeffs_cheb = compute_coeffs(func, nodes_cheb)
     coeffs_uniform = compute_coeffs(func, nodes_uniform)
 
-    print("coffs_cheb : ", coeffs_cheb)
-    print("coffs_uniform : ", coeffs_uniform)
-
-    xs = generate_uniform_nodes(16, 0, 2)
-    y_cheb = []
-    y_uniform = []
-    for x in xs:
-        y_cheb.append(compute_polynomial(x, coeffs_cheb, coeffs_cheb))
-        y_uniform.append(compute_polynomial(x, coeffs_uniform, coeffs_uniform))
-        
-    y_cheb = np.array(y_cheb)
-    y_uniform = np.array(y_uniform)
+    xs = generate_uniform_nodes(32, 0, 2)
+ 
+    y_cheb = compute_polynomials(xs, coeffs_cheb, nodes_cheb)
+    y_uniform = compute_polynomials(xs, coeffs_uniform, nodes_uniform)
     y_real = np.array([func(x) for x in xs])
 
     plt.figure(figsize = (8,6))
@@ -108,5 +117,38 @@ if __name__ == "__main__":
     plt.plot(xs, y_uniform, 'ko-',label = 'interpolate with uniform')
     plt.xlabel("x-axis")
     plt.ylabel("y-axis")
+    plt.ylim([-1.5, 1.5])
     plt.legend()
     plt.savefig("./interpolation.png")
+
+    # use different n_nodes
+
+    n_nodes_list = [4, 8, 16]
+    y_cheb_list = []
+    y_uniform_list = []
+
+    for n_nodes in n_nodes_list:
+        nodes_cheb = generate_chebyshev_nodes(n_nodes, lower, upper, 'type_2')
+        nodes_uniform = generate_uniform_nodes(n_nodes, lower, upper)
+
+        coeffs_cheb = compute_coeffs(func, nodes_cheb)
+        coeffs_uniform = compute_coeffs(func, nodes_uniform)
+
+        y_cheb = compute_polynomials(xs, coeffs_cheb, nodes_cheb)
+        y_uniform = compute_polynomials(xs, coeffs_uniform, nodes_uniform)
+
+        y_cheb_list.append(y_cheb)
+        y_uniform_list.append(y_uniform)
+
+    plt.figure(figsize = (8,6))
+    plt.plot(xs, y_real, 'ro-',label = 'sin(pi * x)')
+
+    for n_nodes, y_cheb, y_uniform in zip(n_nodes_list, y_cheb_list, y_uniform_list):
+        plt.plot(xs, y_cheb, label = "n_nodes : {} cheb".format(n_nodes))
+        plt.plot(xs, y_uniform, label = "n_nodes : {} uniform".format(n_nodes))
+
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.ylim([-1.5, 1.5])
+    plt.legend()
+    plt.savefig("./interpolation_n_nodes.png")
---
+++ README.md
@@ -0,0 +1,5 @@
+# 편미분방정식의 수치해석
+### Index
+1. lecture 01 : Computer Architecture
+2. lecture 02 : Interpolation
+3. lecture 03 : Differentiation and Integration
