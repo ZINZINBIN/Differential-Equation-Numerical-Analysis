cca6db15a9721bb6a8f83a628b4b07badd89862c
zinzinbin <asdwlstn@naver.com>
2022-11-13 18:02:41 +0000

    update homework 02

--- HW02/homework.py
+++ HW02/homework.py
@@ -18,6 +18,25 @@ import math
 from tqdm.auto import tqdm
 from typing import List, Callable, Union, Literal, Optional
 
+def FDM_prob01(xs : np.array, dx : float):
+    nx = len(xs)
+    A = np.zeros((nx,nx))
+    
+    for idx_x in range(1,nx-1):
+        A[idx_x,idx_x] = (-1) * (2 + (xs[idx_x] * dx)**2)
+        A[idx_x,idx_x+1] = 1
+        A[idx_x,idx_x-1] = 1
+    
+    idx_x = 0
+    A[idx_x, idx_x] = (-1) * (2 + (xs[idx_x]* dx)**2)
+    A[idx_x, idx_x+1] = 1
+        
+    idx_x = len(xs) - 1
+    A[idx_x, idx_x] = (-1) * (10/3 + (xs[idx_x]* dx)**2)
+    A[idx_x, idx_x-1] = 4/3
+    
+    return A
+
 def FDM_prob02(nx : int, xl : float, xr : float, dt : float, m : int):
     dx = (xr - xl) / nx
     xs = np.linspace(xl, xr, nx)
@@ -25,21 +44,58 @@ def FDM_prob02(nx : int, xl : float, xr : float, dt : float, m : int):
     
     for idx_x in range(1,nx-1):
         A[idx_x,idx_x] = 1
-        A[idx_x,idx_x+1] = xs[idx_x] * m * dt / 2 / dx
-        A[idx_x,idx_x-1] = (-1) * xs[idx_x] * m * dt / 2 / dx
+        A[idx_x,idx_x+1] = (-1) * xs[idx_x] * m * dt / 2 / dx
+        A[idx_x,idx_x-1] =  xs[idx_x] * m * dt / 2 / dx
     
     idx_x = 0
-    A[idx_x, idx_x] = 1 - 3 * xs[idx_x] * m * dt / 2 / dx
-    A[idx_x, idx_x+1] = 4 * xs[idx_x] * m * dt / 2 / dx
-    A[idx_x, idx_x+2] = (-1) * xs[idx_x] * m * dt / 2 / dx
+    A[idx_x, idx_x] = 1 + 3 * xs[idx_x] * m * dt / 2 / dx
+    A[idx_x, idx_x+1] = (-1) * 4 * xs[idx_x] * m * dt / 2 / dx
+    A[idx_x, idx_x+2] = xs[idx_x] * m * dt / 2 / dx
         
     idx_x = len(xs) - 1
-    A[idx_x, idx_x] = 1 + xs[idx_x] * m * dt / 2 / dx
-    A[idx_x, idx_x-1] = (-4) * xs[idx_x] * m * dt / 2 / dx
-    A[idx_x, idx_x-2] = 3 * xs[idx_x] * m * dt / 2 / dx
+    A[idx_x, idx_x] = 1 - 3 * xs[idx_x] * m * dt / 2 / dx
+    A[idx_x, idx_x-1] = 4 * xs[idx_x] * m * dt / 2 / dx
+    A[idx_x, idx_x-2] = (-1) * xs[idx_x] * m * dt / 2 / dx
     
     return A
 
+# Tridiagonal Gauss Elimination for problem 01
+def Tri_Gauss_Elimination(A_origin : np.ndarray, B_origin : np.array):
+    A = np.copy(A_origin)
+    B = np.copy(B_origin)
+    x = np.zeros_like(B_origin)
+    
+    m,n = A.shape
+    
+    # Forward Elimination
+    for idx in range(m-1):
+        B[idx+1] -= B[idx] * A[idx+1,idx] / A[idx,idx]
+        A[idx+1,idx] = 0
+        A[idx+1,idx+1] -= A[idx,idx+1] * A[idx+1,idx] / A[idx,idx]
+        
+    # Backward substitution
+    for idx in reversed(range(m)):
+        if idx == m-1:
+            x[idx] = B[idx] / A[idx,idx]
+        else:
+            x[idx] = (B[idx] - A[idx,idx+1] * x[idx+1]) / A[idx,idx]
+    
+    return x
+
+# Boundary correction for problem 01
+def Boundary_correction(x : np.array):
+    x_cor = np.zeros(len(x) + 2)
+    
+    # x = 0 -> u(0,t) = sin(t), X(x=0) = 1
+    x_cor[0] = 1
+    x_cor[-1] = x[-1]
+    
+    for idx in range(len(x)):
+        x_cor[idx+1] = x[idx]
+
+    return x_cor
+    
+# PDE solver for problem 02
 class PDESolver:
     def __init__(
         self, 
@@ -102,10 +158,50 @@ class PDESolver:
         plt.legend()
         plt.title(self.title)
         plt.savefig(self.save_dir)
+        
+    def update_parameters(self, **kwargs):
+        
+        allowed_keys = ['A', 'save_dir', 'title']
+        
+        if kwargs is not None:
+            self.__dict__.update((k,v) for k,v in kwargs.items() if k in allowed_keys)
 
 if __name__ == "__main__":
     
-    # problem 02 : m = 1 case
+    # problem 01 
+    xl = 0; xr = 1; nx = 64; dx = (xr-xl) / nx; xs = np.linspace(xl,xr,nx)
+    A = FDM_prob01(xs[1:nx-1], dx); B = np.zeros_like(xs[1:nx-1]); B[0] = -1
+    
+    # solve differential equation
+    X = Tri_Gauss_Elimination(A,B)
+    
+    # add boundary condition
+    X = Boundary_correction(X)
+    
+    # time component
+    nt = 64; ts = np.linspace(0, 2 * np.pi, nt); T = np.sin(ts)
+    
+    # plot problem 01
+    fig, axes = plt.subplots(nrows = 1, ncols = 2, figsize=(10, 5))
+    
+    axes[0].plot(xs, X)
+    axes[0].set_title("Spatial part of u(x,t)")
+    axes[0].set_xlabel("0<x<1")
+    axes[1].set_ylabel("spatial part : X(x)")
+    
+    xs, ts = np.meshgrid(xs,ts, indexing='ij')
+    u_mesh = np.matmul(X.reshape(-1,1), T.reshape(1,-1))
+    pcr = axes[1].pcolor(ts,xs, u_mesh)
+    fig.colorbar(pcr, ax = axes[1], extend = 'max')
+    axes[1].set_title("plot for u(x,t) with t vs x")
+    axes[1].set_xlabel("0<t<2pi")
+    axes[1].set_ylabel("0<x<1")
+    
+    fig.tight_layout()
+    plt.savefig("./problem01.png")
+    
+    # problem 02
+    # m = 1 case
     m = 1; L = 10; nx = 256; xl = (-1) * L; xr = L; dt = 1e-3; x0 = 1e-2
     A = FDM_prob02(nx, xl, xr, dt, m)
     
@@ -134,11 +230,65 @@ if __name__ == "__main__":
         lambda x : Boundary_prob02(x, xs),
         lambda x : Initialize_prob02(x, xs, x0),
         A,
-        save_dir = "./problem02.png",
-        title = "u(x,t) graph with different t",
+        save_dir = "./problem02_01.png",
+        title = "u(x,t) graph with different t (m=1)",
         plot_time_index=[0,int(len(ts)/2), len(ts) -1]
     )
     
     solver.solve()
     
+    solver.plot()
+    
+    # m = -1 case
+    solver.update_parameters(A=FDM_prob02(nx, xl, xr, dt, -1), title = "u(x,t) graph with different t (m=-1)", save_dir = "./problem02_02.png")
+    
+    solver.solve()
+    
+    solver.plot()
+    
+    
+    # different x0
+    # case : m = 1
+    x0 = 1e-1
+    solver.update_parameters(
+        Initialize = lambda x : Initialize_prob02(x, xs, x0),
+        A=FDM_prob02(nx, xl, xr, dt, m), 
+        title = "u(x,t) graph with different t (m=1, x0=0.1)", 
+        save_dir = "./problem02_03.png"
+    )
+    solver.solve()
+    solver.plot()
+    
+    x0 = 1
+    solver.update_parameters(
+        Initialize = lambda x : Initialize_prob02(x, xs, x0),
+        A=FDM_prob02(nx, xl, xr, dt, m), 
+        title = "u(x,t) graph with different t (m=1, x0=1.0)", 
+        save_dir = "./problem02_04.png"
+    )
+    
+    solver.solve()
+    solver.plot()
+    
+    # case : m = -1
+    x0 = 1e-1
+    m = -1
+    solver.update_parameters(
+        Initialize = lambda x : Initialize_prob02(x, xs, x0),
+        A=FDM_prob02(nx, xl, xr, dt, m), 
+        title = "u(x,t) graph with different t (m=-1, x0=0.1)", 
+        save_dir = "./problem02_05.png"
+    )
+    solver.solve()
+    solver.plot()
+    
+    x0 = 1
+    solver.update_parameters(
+        Initialize = lambda x : Initialize_prob02(x, xs, x0),
+        A=FDM_prob02(nx, xl, xr, dt, m), 
+        title = "u(x,t) graph with different t (m=-1, x0=1.0)", 
+        save_dir = "./problem02_06.png"
+    )
+    
+    solver.solve()
     solver.plot()
